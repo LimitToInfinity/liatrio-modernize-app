b98928548066fc04780653e9666ec15b744405eb
LimitToInfinity <21stLimitless@gmail.com>
2025-03-13 15:46:31 -0600

    Add Things rendering in JSON and Cards with completion handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,90 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import "./App.scss";
+
+const apiUrl = 'http://localhost:3000/';
+
+export interface Thing {
+  id: number;
+  title: string;
+  price: number;
+  completed: boolean;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [thingsObject, setThingsObject] = useState<Thing[]>([]);
+  const [things, setThings] = useState<Thing[]>([]);
+
+  const fetchThings = async () => {
+    const response = await fetch(`${apiUrl}things`);
+    const result = await response.json();
+
+    setThingsObject(result);
+
+    const apiThingsObject = JSON.parse(result.things_stored);
+    const apiThings: Thing[] = Object.values(apiThingsObject);
+    setThings(apiThings);
+  }
+
+  useEffect(() => {
+    fetchThings();
+  }, []);
+
+  const toggleCompleteThing = async (id: number, completed: boolean) => {
+    const response = await fetch(`${apiUrl}/things/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ completed }),
+    });
+    const updatedThing = await response.json();
+    setThings(things.map((thing) => (thing.id === id ? updatedThing : thing)));
+  };
+
+  const renderThings = () => {
+    return things.map((thing) => (
+      <Card
+        key={thing.id}
+        border={ thing.completed ? 'success' : 'danger' }
+        style={{ width: '20%', minWidth: '10rem', marginRight: '1rem', marginTop: '1rem', borderWidth: '2px' }}
+      >
+        <Card.Body>
+          <Card.Title>{thing.title}</Card.Title>
+          <Card.Subtitle>{thing.completed ? 'completed' : 'awaiting'}</Card.Subtitle>
+          <Card.Text>Price {thing.price}</Card.Text>
+          <Button
+              variant={thing.completed ? 'outline-danger' : 'outline-success'}
+              onClick={() => toggleCompleteThing(thing.id, !thing.completed)}
+          >        
+              {thing.completed ? 'Undo' : 'Complete'}
+          </Button>
+        </Card.Body>
+      </Card>
+    ));
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <Navbar expand="lg" bg="dark" data-bs-theme="dark" className="bg-body-tertiary">
+          <Container>
+            <Navbar.Brand href="#home">Liatrio Modernize It</Navbar.Brand>
+          </Container>
+        </Navbar>
+      </header>
+      <main>
+          <h1>Things</h1>
+          <div>{JSON.stringify(thingsObject)}</div>
+          <Button onClick={fetchThings}>Fetch Things</Button>
+          {renderThings()}
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
